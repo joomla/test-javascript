bbb9b7871605f69ea39eb56d3fd5a94dcee2d14b
Dimitri Grammatikogianni <mit505@upshift.gr>
2018-03-30 11:17:18 +0200

    Vanilla jasmin (#1)
    * update deps
    
    * Simplify JS tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,48 +1,39 @@
 // Karma configuration
-
 module.exports = function (config) {
 	config.set({
-
 		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: '',
+		basePath: __dirname.replace('node_modules/joomla-javascript-tests/src', ''),
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['jasmine-ajax', 'jasmine', 'requirejs'],
+		frameworks: ['fixture', 'jasmine'],
+
+		//
 
 		// list of files / patterns to load in the browser
 		files: [
-			{pattern: 'node_modules/jquery/dist/jquery.min.js', included: false},
-			{pattern: 'node_modules/jasmine-jquery/lib/jasmine-jquery.js', included: false},
-			{pattern: 'node_modules/text/text.js', included: false},
-			{pattern: 'media/vendor/bootstrap/js/bootstrap.min.js', included: false},
-			{pattern: 'media/vendor/jquery-ui/js/jquery.ui.core.min.js', included: false},
-			{pattern: 'media/vendor/jquery-ui/js/jquery.ui.sortable.min.js', included: false},
-			{pattern: 'media/system/js/*.js', included: false},
-			{pattern: 'media/system/js/legacy/*.js', included: false},
-			{pattern: 'media/system/js/fields/*.js', included: false},
-			{pattern: 'media/system/js/polyfills/webcomponents/webcomponents-ce.min.js', included: true},
-			{pattern: 'media/system/webcomponents/joomla-alert.min.js', included: true},
-			{pattern: 'media/system/js/fields/calendar-locales/*.js', included: false},
-			{pattern: 'media/system/js/fields/calendar-locales/date/gregorian/*.js', included: false},
-			{pattern: 'tests/javascript/**/fixture.html', included: false},
-			{pattern: 'tests/javascript/**/spec.js', included: false},
-			{pattern: 'tests/javascript/**/spec-setup.js', included: false},
-			{pattern: 'images/*.png', included: false},
+			'./node_modules/joomla-javascript-tests/src/**/fixtures/*.html',
 
-			'tests/javascript/test-main.js'
+			// 'node_modules/karma-fixture-loader/karma-fixture-loader.js',
+			{ pattern: 'media/legacy/js/caption.js', loaded: true, served: true, watch: false },
+
+
+			'node_modules/joomla-javascript-tests/src/caption/*.js',
+
 		],
 
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'**/system/js/*.js': ['coverage']
+			'**/system/js/*.js': ['coverage'],
+			'**/*.html': ['html2js'],
+			'**/*.json': ['json_fixtures'],
 		},
 
 		// coverage reporter configuration
 		coverageReporter: {
-			type : 'html',
-			dir : 'build/coverage-js/'
+			type: 'html',
+			dir: 'build/coverage-js/'
 		},
 
 		// test results reporter to use
@@ -58,7 +49,7 @@ module.exports = function (config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+		logLevel: config.INFO,
 
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: true,
@@ -73,11 +64,12 @@ module.exports = function (config) {
 
 		// list of plugins
 		plugins: [
+			'karma-fixture',
+			'karma-html2js-preprocessor',
+			'karma-json-fixtures-preprocessor',
 			'karma-jasmine',
-			'karma-jasmine-ajax',
 			'karma-firefox-launcher',
 			'karma-coverage',
-			'karma-requirejs',
 			'karma-verbose-reporter'
 		],
 
