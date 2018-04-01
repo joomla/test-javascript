67beaed9c394c06131d079e8af4e558f11affea8
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2018-04-01 15:16:43 +0200

    fixit

--- src/joomla-switcher/joomla-switcher.spec.js
+++ src/joomla-switcher/joomla-switcher.spec.js
@@ -8,7 +8,7 @@
  * @since       4.0.0
  * @version     1.0.0
  */
-describe('JCaption applied to single image', () => {
+describe('Joomla switcher tests', () => {
 	// Set up the script
 	beforeAll(() => {
 		fixture.setBase('node_modules/joomla-javascript-tests/src/joomla-switcher')
@@ -20,9 +20,48 @@ describe('JCaption applied to single image', () => {
 		fixture.cleanup();
 	});
 
-	describe('Joomla switcher default true', () => {
-		it('Should have the true value selected', () => {
-			expect(fix.querySelector('#single').querySelector('p').innerHTML === 'Joomla Title 1').toBe(true);
+	describe('Joomla switcher first option preselected', () => {
+		it('Should have a constructor name: JoomlaSwitcherElement', () => {
+			const switcher = fix.querySelector('joomla-field-switcher');
+			expect(switcher.constructor.name).toBe('JoomlaSwitcherElement');
+		});
+
+		it('Joomla switcher have 5 spans inside', () => {
+			console.log(fix)
+			const switcher = fix.querySelector('joomla-field-switcher');
+			const spans = [].slice.call(switcher.querySelectorAll('span'));
+			expect(spans.length).toBe(5);
+		});
+
+		it('Joomla switcher have 1st span with aria-checked true', () => {
+			const switcher = fix.querySelector('joomla-field-switcher');
+			const span = switcher.querySelector('span.switcher');
+			expect(span.getAttribute('aria-checked')).toBe('true');
+		});
+
+		it('Joomla switcher have 1st span to have class active', () => {
+			const switcher = fix.querySelector('joomla-field-switcher');
+			const span = switcher.querySelector('span.switcher-label-0');
+			expect(span.classList.contains('active')).toBe(true);
+		});
+
+		it('Joomla switcher have 2nd span not to have class active', () => {
+			const switcher = fix.querySelector('joomla-field-switcher');
+			const span = switcher.querySelector('span.switcher-label-1');
+			expect(span.classList.contains('active')).toBe(false);
+		});
+
+		it('Joomla switcher on click change state', () => {
+			const clickable = fix.querySelector('input#sw1');
+			const switcher = fix.querySelector('joomla-field-switcher');
+			const span0 = switcher.querySelector('span.switcher-label-0');
+			const span1 = switcher.querySelector('span.switcher-label-1');
+			const spanA = switcher.querySelector('span.switcher');
+			clickable.click();
+			expect(spanA.getAttribute('aria-checked')).toBe('true');
+			expect(span0.classList.contains('active')).toBe(false);
+			expect(span1.classList.contains('active')).toBe(true);
 		});
 	});
+
 });
